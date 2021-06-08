65ad29efb956c7fbef169ae7307aef357ddb868a
inoy <7m7110123@gmail.com>
2021-06-08 18:06:08 +0900

    number型、string型

---
+++ src/number-string.ts
@@ -0,0 +1,12 @@
+export {};
+
+let year: number = 1999;
+// year = false;
+console.log({ year });
+
+const age: number = 0x2b;
+console.log({ age });
+
+let name: string = 'Ham';
+// name = 10;
+console.log({ name });
