20385b3e2ff42ac4d9dfeb85887f8a7635331d52
inoy <7m7110123@gmail.com>
2021-06-09 08:59:09 +0900

    array型

---
+++ src/array.ts
@@ -0,0 +1,10 @@
+export {};
+
+const numbers: number[] = [1, 2, 3];
+const numbers2: Array<number> = [1, 2, 3];
+
+const strings: string[] = ['TypeScript', 'JavaScript', 'CoffeeScript'];
+
+const nijigenHairetsu: number[][] = [[50, 100], [150, 300], []];
+
+const hairetsu: (string | number | boolean)[] = [1, false, 'Japan'];
